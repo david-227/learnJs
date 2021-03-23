6593916164f20ab8e82ec3c5d2ec2eb91c348ff3
david-227 <david_sp@outlook.com>
2021-03-23 17:45:24 -0400

    Uploading practice files

---
+++ home.js
@@ -0,0 +1,13 @@
+// variables
+/* It is prefereable to use let and const over var in new JS
+let is used when a variable can be changed
+const is for variables that are not changed
+*/
+
+let pizzaBoy = 'dave'
+console.log(pizzaBoy)
+pizzaBoy = 'boony'
+console.log(pizzaBoy)
+
+const pizzaMan = 'humongo'
+console.log(pizzaMan)
